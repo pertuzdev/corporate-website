e536cb4a2c748ca1dc6e7337c9b782950e2bc051
pertuzdev <antonio.pertuz01@gmail.com>
2022-01-28 10:14:26 -0600

    Organize images files

--- src/views/AboutPage/GoalsSection/index.js
+++ src/views/AboutPage/GoalsSection/index.js
@@ -19,19 +19,21 @@ export default function GoalsSection() {
           <GoalsWrapper>
             <GoalsTitle color="white">Misión</GoalsTitle>
             <GoalsDescription color="white">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Accusamus deleniti dolor impedit, incidunt laborum nisi nobis quas
-              sequi. At id, iure nihil quod recusandae similique suscipit.
-              Beatae consequatur corporis soluta.
+              Ofrecer y entregar soluciones tecnológicas creativas e
+              innovadoras, adaptables a las necesidades de nuestros clientes en
+              EL Salvador y Centroamerica, fomentando su desarrollo y
+              productividad, mediante un equipo de profesionales en TIC
+              altamente competitivo.
             </GoalsDescription>
           </GoalsWrapper>
           <GoalsWrapper>
             <GoalsTitle color="white">Visión</GoalsTitle>
             <GoalsDescription color="white">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Accusamus deleniti dolor impedit, incidunt laborum nisi nobis quas
-              sequi. At id, iure nihil quod recusandae similique suscipit.
-              Beatae consequatur corporis soluta.
+              Ser una empresa competitiva que innove a la región en servicios y
+              productos de TI que mejoren notablemente la productividad de las
+              PYMES, reconocida en la región centroamericana, por las soluciones
+              que entregamos, generando relaciones duraderas con clientes,
+              proveedores y nuestra gente.
             </GoalsDescription>
           </GoalsWrapper>
         </GoalsSectionWrapper>
